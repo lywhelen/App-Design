f5bdd25274ada0724c73a035344e0510c4d42789
Vaishnavi <svbachiraju@gmail.com>
2026-04-25 18:33:03 -0400

    added Task table

--- db.py
+++ db.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Interval
+from datetime import timedelta
 
 db = SQLAlchemy()
 
@@ -16,7 +18,6 @@ class User(db.Model):
 
     tasks = db.relationship(
         "Task",
-        secondary=tasks,
         cascade="delete"
     )
 
@@ -47,9 +48,48 @@ class User(db.Model):
             body["tasks"] = None
         return body
 
-
+#notifiications 
 class Task(db.Model):
     """
     Assignment model
     """
     __tablename__ = "tasks"
+    #id, tiitle, description, priority, estimated time, ocmpleted
+    id=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String, nullable=True)
+    description=db.Column(db.String, nullable=False)
+    priority=db.Column(db.Integer, nullable=True) #scale from 1 to 10
+    duration=db.Column(Interval)
+    user_id=db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    def __init__(self, **kwargs):
+        self.title = kwargs.get("title", "")
+        self.description = kwargs.get("description")
+        self.priority = kwargs.get("priority", 1)
+        self.duration = kwargs.get("duration", timedelta(hours=0, minutes=30))
+        self.user_id = kwargs.get("user_id")
+        
+
+
+    def serialize(self, include_tasks=True):
+        """
+        Serialize a user object
+        """
+        body = {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "priority": self.priority,
+            "duration": self.duration,
+            "user_id": self.user_id
+        }
+        return body
+
+
+
+
+#example for creatigna   new task, new_task=Task(duration=timedelta(hours=2, minutes=30))
+
+
+
+
