390cd219212024d50374992a04eb2f8229636904
Vaishnavi <svbachiraju@gmail.com>
2026-04-25 18:52:38 -0400

    added base for app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+from db import db
+from flask import Flask
+from flask import request
+import json
+
+app = Flask(__name__)
+db_filename = "game.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename 
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+#craetes dattabase
+db.init_app(app)
+with app.app_context():
+    db.create_all() #createts aall the tables we will defien in db.py
+
+def success_response(data, code=200):
+    return json.dumps({"success": True, "data": data}), code
+
+def failure_response(message, code=404):
+    return json.dumps({"success": False, "error": message}), code
+
+#@app.route("/")
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
